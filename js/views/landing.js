f9f46943d0c10e5214f815c748f26b50ad9b585a
Vullkan92 <vullkan.halili@riinvest.net>
2016-01-08 10:54:02 +0100

    minor changes

--- js/views/landing.js
+++ js/views/landing.js
@@ -4,12 +4,12 @@ window.HomeView = Backbone.View.extend({
       this.render();
     },
     render: function(){
-      var apple = new Option({ name: "Apple", id: "apple", image_url: "http://www.tapgamers.com/wp-content/uploads/2011/12/apple-250x250.jpg"});
-      var banana = new Option({ name: "Banana", id: "banana", image_url: "http://thefruiterer.com.au/wp-content/uploads/2013/05/Banana-ripe-250x250.jpg"});
-      var durian = new Option({ name: "Durian", id: "durian", image_url: "http://3.imimg.com/data3/NW/AY/MY-9798726/durian-fruit-250x250.jpg"});
-      var orange = new Option({ name: "Orange", id: "orange", image_url: "http://2.imimg.com/data2/AQ/RA/MY-5878576/orange-250x250.jpg"});
-      var pear = new Option({ name: "Pear", id: "pear", image_url: "http://3.imimg.com/data3/NV/LA/MY-9833507/pear-fruit-250x250.jpg"});
-      var peach = new Option({ name: "Peach", id: "peach", image_url: "http://img2.everychina.com/img/1b/bc/2ce0607f7e4756adb95cb769e89b-250x250c1-5d9d/frozen_peach_fresh_frozen_fruit_ad_fd_fruit.jpg"});
+      var apple = new Option({ name: "Apple", id: "apple", image_url: "static/css/images/apple.jpg"});
+      var banana = new Option({ name: "Banana", id: "banana", image_url: "static/css/images/banana.jpg"});
+      var durian = new Option({ name: "Durian", id: "durian", image_url: "static/css/images/durian.jpg"});
+      var orange = new Option({ name: "Orange", id: "orange", image_url: "static/css/images/orange.jpg"});
+      var pear = new Option({ name: "Pear", id: "pear", image_url: "static/css/images/pear.jpg"});
+      var peach = new Option({ name: "Peach", id: "peach", image_url: "static/css/images/peach.jpg"});
 
       var myOptions = new Options([apple, banana, durian, orange, pear, peach]);
 
